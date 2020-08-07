fcc1de6a3e5b8cd81cf4bd65fb507fd609009b17
tejt99 <50763476+tejt99@users.noreply.github.com>
2020-08-07 09:23:31 +0100

    Delete loading-attribute-polyfill.min.js

--- loading-attribute-polyfill.min.js
+++
@@ -1,5 +0,0 @@
-/*
- * Loading attribute polyfill - https://github.com/mfranzke/loading-attribute-polyfill
- * @license Copyright(c) 2019 by Maximilian Franzke
- * Credits for the initial kickstarter / script to @Sora2455, and supported by @cbirdsong, @eklingen, @DaPo, @nextgenthemes, @diogoterremoto, @dracos, @Flimm, @TomS- and @vinyfc93 - many thanks for that !
- */
